861811fa1fcf2dd9e65ca36ab9929992607b4377
Andrew Shewchenko <andrew.shewchenko@gmail.com>
2016-05-29 17:58:10 -0400

    Moves config files into cookbook

--- ashew-flask/cookbooks/ashew-flask/recipes/default.rb
+++ ashew-flask/cookbooks/ashew-flask/recipes/default.rb
@@ -11,5 +11,43 @@ apt_package 'nginx'
 apt_package 'git'
 apt_package 'gcc'
 
-include_recipe 'python'
-python_pip 'Flask==0.10.1'
+bash 'install_flask' do
+	code <<-EOH
+		pip install 'Flask==0.10.1'
+		EOH
+  ignore_failure
+end
+
+cookbook_file '/etc/gunicorn.d/ashew-flask.conf' do
+  source 'etc/gunicorn.d/ashew-flask.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, 'service[gunicorn]', :delayed
+end
+
+cookbook_file '/etc/nginx/sites-available/ashew-flask.conf' do
+  source 'etc/nginx/sites-available/ashew-flask.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+link '/etc/nginx/sites-enabled/ashew-flask.conf' do
+  to '/etc/nginx/sites-available/ashew-flask.conf'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+end
+
+service 'gunicorn' do
+  action :restart
+  ignore_failure
+end
+
+service 'nginx' do
+  action :restart
+  ignore_failure
+end
