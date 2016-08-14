dee3b75bdbacd4de16d92a0cb4a9c771d6044b55
Hecber Cordova <hecber@gmail.com>
2016-08-13 23:34:30 -0400

    Added initial script, user-data and playbook

---
+++ user_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt-add-repository ppa:ansible/ansible -y
+apt-get update -y
+apt-get install ansible -y
+apt-get install git -y
+mkdir /usr/local/repo
+git clone https://github.com/skysec/aws-ansible-lab.git /usr/local/repo
+cd /usr/local/repo/playbook
+ansible-playbook site.yml
