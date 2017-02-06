8ce007fc2eda50c415bafb9bcc09e1f8e5b4e5eb
Matt Schurenko <mschurenko@demonware.net>
2017-02-05 21:28:53 -0500

    adding ALB to cfn

--- aws-app.sh
+++ aws-app.sh
@@ -2,6 +2,10 @@
 
 # aws-app.sh
 
+# Requirements:
+#  - awscli >= 1.11.44
+#  - docker
+
 # This scripts is broken up into three stages:
 # - Infrastructue
 # - Application Build
@@ -20,6 +24,13 @@
 # Improvements:
 # - DNS name
 # - https instead of http
+# - Dockerfile should live in app repo
+# used git sha's as in this scenerio I do not have access to the app repo. using git tags would be better
+
+# To do:
+#  - add docker reg to cfn
+#  - add ECS cluster to cfn
+#  - auto create first task definition and service
 
 set -eu
 #set -x
@@ -29,14 +40,43 @@ USAGE="\nUsage:$0 [--destroy|-d]\n"
 #GIT_REPO="https://github.com/wvchallenges/opseng-challenge-app.git"
 GIT_REPO="https://github.com/mschurenko/opseng-challenge-app.git"
 REPO_DIR="opseng-challenge-app"
-DOCKER_REG="505545132866.dkr.ecr.us-east-1.amazonaws.com/mschurenko-challenge-app"
 APP_NAME="opseng-challenge-app"
-ECS_SERVICE="myapp"
+ALB_NAME="mschurenko-alb"
+#ALB_TARGET_GROUP="arn:aws:elasticloadbalancing:us-east-1:505545132866:targetgroup/ecs-mschur-myapp/7803dbaee8ffa693"
+ALB_TARGET_GROUP="arn:aws:elasticloadbalancing:us-east-1:505545132866:targetgroup/mschurnenko-ecs-service/2c2ac7d975a213fc"
 ECS_CLUSTER="mschurenko-test"
+ECS_SERVICE="mschurenko-servicev2"
+ECS_DESIRED_COUNT=1
+ECS_TASK_FAMILY="mschurenko-task"
+ECS_CONTAINER_PORT=8000
+IAM_ROLE="arn:aws:iam::505545132866:role/ecsServiceRole"
+DOCKER_REG="505545132866.dkr.ecr.us-east-1.amazonaws.com/mschurenko-challenge-app"
 
 export AWS_DEFAULT_OUTPUT=text
 
 ## Functions ##
+b_print() {
+    local msg="$1"
+    echo -e "\e[96m${msg}\e[0m"
+}
+
+g_print() {
+    echo -e "\e[92mUnchanged\e[0m"
+}
+
+y_print() {
+    echo -e "\e[93mChanged\e[0m"
+}
+
+r_print() {
+    echo -e "\e[91mFailed\e[0m"
+}
+
+p_stage() {
+    local msg="$1"
+    echo -e "\e[4;100mStage: $msg\e[0m"
+}
+
 sanity_checks() {
     set +u
     if [[ -z $AWS_DEFAULT_REGION ]] || [[ -z $AWS_ACCESS_KEY_ID ]] || \
@@ -69,8 +109,9 @@ destroy_infra() {
     echo "Destroying infrastructure..."
     exit
 }
-
-# main #
+#-------
+# main
+#-------
 sanity_checks
 
 if [[ -n $@ ]];then
@@ -84,94 +125,129 @@ if [[ -n $@ ]];then
                 echo "Skipping destroy of infrastructure.";exit
             fi
         ;;
-        --help|-h) echo -e $USAGE;exit;;
-        *) echo -e $USAGE;exit 1
+        --help|-h) echo -e "$USAGE";exit;;
+        *) echo -e "$USAGE";exit 1
         ;;
     esac
 fi
 
 BASE_DIR=$(pwd)
 
-## Stages ##
+#----------------------
+# Infrastructure Stage
+#----------------------
+p_stage "Infrastructure"
 
-### Infrastructure ###
-cd cfn
 stack_name="mschurenko-VPC"
 stack_status=$(get_stack_status $stack_name)
 
-echo $stack_status
+echo "Checking if we need to build infrastructure..."
 
-# if [[ $stack_status == "CREATE_COMPLETE" ]] || [[ $stack_status == "UPDATE_COMPLETE" ]];then
+# cd infra
+# if [[ $stack_status =~ _COMPLETE$ ]];then
 #     aws cloudformation validate-template --template-body file://./vpc.yaml
-#     aws cloudformation update-stack \ #     --template-body file://./vpc.yaml \
-#     --stack-name "${name}-VPC" \
+#     aws cloudformation update-stack \
+#     --template-body file://./vpc.yaml \
+#     --stack-name $stack_name \
 #     --capabilities CAPABILITY_IAM \
-#     --tags Key=Name,Value=${name}
+#     --tags Key=Name,Value=$stack_name
+#     echo "Waiting for stack update on $stack_name to complete..."
+#     aws cloudformation wait stack-update-complete --stack-name $stack_name
 # else
-#     echo "$stack_name already exists"
+#     echo "$stack_name not in a state where it can be updated. Status is $stack_status"
+#     echo "State: $(r_print)"
+#     exit 4
 # fi
 # aws cloudformation wait stack-exists --stack-name $stack_name
 # wait until cluster state is ACTIVE
 # cluster_state=$(aws ecs describe-clusters --clusters mschurenko-test|awk '/^CLUSTERS/ {print $NF}')
 
 cd $BASE_DIR
-
-### Application Build ###
+#--------------
+# Build Stage
+#--------------
+p_stage "Build"
+echo "Checking if we need to build/deploy..."
 if [[ ! -d $REPO_DIR ]];then
-    git clone $GIT_REPO $REPO_DIR
-    build_image=true
+    git clone $GIT_REPO $REPO_DIR &>/dev/null
     git_sha=$(git rev-parse --short HEAD)
 else
     cd $REPO_DIR
-    git pull
+    git pull &>/dev/null
     git_sha=$(git rev-parse --short HEAD)
 fi
 
-echo "git sha: $git_sha"
+docker_image=${DOCKER_REG}:${git_sha}
+deploy=false
 
 # compare git SHA with deployed container
-cur_task_def=$(aws ecs describe-services --service $ECS_SERVICE --cluster $ECS_CLUSTER\
-|awk '/^SERVICES/ {print $NF}')
-echo "Current task def: $cur_task_def"
-docker_label=$(aws ecs describe-task-definition --task-definition $cur_task_def\
-|awk '/^DOCKERLABELS/ {print $2}')
-echo "Current docker label: $docker_label"
+set +e
+service=$(aws ecs list-services --cluster $ECS_CLUSTER|grep $ECS_SERVICE)
+set -e
+echo "service: $service"
+if [[ -n $service ]];then
+    service_exists=true
+    cur_task_def=$(aws ecs describe-services --service $ECS_SERVICE --cluster $ECS_CLUSTER\
+    |awk '/^SERVICES/ {print $NF}')
+    cur_img=$(aws ecs describe-task-definition --task-definition $cur_task_def\
+    |awk '/^CONTAINERDEFINITIONS/ {print $4}')
+else
+    service_exists=false
+    cur_img=""
+    deploy=true
+fi
+
+echo "service exists: $service_exists"
 
 cd $BASE_DIR
-if [[ $docker_label != $git_sha ]];then
-    echo "$docker_label and $git_sha don't match"
-    ## Build Applicaiton ##
+if [[ -z $cur_img ]] || [[ $cur_img != $docker_image ]];then
+    b_print "Building ${DOCKER_REG}:${git_sha}..."
     docker build -t ${APP_NAME}:${git_sha}\
     --build-arg repo=${REPO_DIR} \
     --build-arg version=${git_sha} .
-    docker tag ${APP_NAME}:${git_sha} ${DOCKER_REG}:${git_sha}
+    docker tag ${APP_NAME}:${git_sha} $docker_image
     $(aws ecr get-login)
-    docker push ${DOCKER_REG}:${git_sha}
+    docker push $docker_image
     cd $BASE_DIR
+    echo "Status: $(y_print)"
+    deploy=true
+else
+    echo "Status: $(g_print)"
+fi
 
-    ## Deploy Appication ##
-    cd ecs
-    # set name, image and dockerlabel
+#--------------
+# Deploy Stage
+#--------------
+p_stage "Deploy"
+if [[ $deploy == "true" ]];then
+    b_print "Deploying $docker_image"
+    cd deploy
     temp_json=${$}_tmp.json
-    ../utils/set-task-def.py test ${DOCKER_REG}:${git_sha} $git_sha < task-definition.json > $temp_json
+    # creating a task def is idempotent
+    ${BASE_DIR}/utils/set-task-def.py $ECS_TASK_FAMILY $APP_NAME $docker_image \
+    < task-def-skel.json > $temp_json
     task_def=$(aws ecs register-task-definition --cli-input-json file://./${temp_json}\
     |awk '/^TASKDEFINITION/ {print $NF}')
     command rm $temp_json
 
-    echo "New task def: $task_def"
-
-    echo "Updating Service $ECS_SERVICE"
-    aws ecs update-service --cluster $ECS_CLUSTER --service $ECS_SERVICE \
-    --task-definition $task_def|grep ^DEPLOYMENTS
+    if [[ $service_exists == "true" ]];then
+        echo "HERE!!!"
+        aws ecs update-service \
+        --cluster $ECS_CLUSTER \
+        --service $ECS_SERVICE \
+        --task-definition $task_def|grep ^DEPLOYMENTS
+    else
+        aws ecs create-service \
+        --cluster $ECS_CLUSTER \
+        --service $ECS_SERVICE \
+        --task-definition $task_def \
+        --desired-count $ECS_DESIRED_COUNT \
+        --load-balancers \
+        containerName=${APP_NAME},containerPort=${ECS_CONTAINER_PORT},targetGroupArn=${ALB_TARGET_GROUP} \
+        --role $IAM_ROLE
+    fi
     # wait for service to be healthy
+    g_print "$docker_image has been deployed to $ECS_SERVICE"
 else
-    echo "ECS is up-to-date with $REPO_DIR"
+    echo "Status: $(g_print)"
 fi
-#cd ecs
-# create task definition
-# create service
-#task_def=$(aws --output text ecs register-task-definition --cli-input-json \
-#file://./task-definition.json|awk '/^TASKDEFINITION/ {print $NF}')
-#echo $task_def
-#aws ecs update-service --cluster mschurenko-test --service myapp \
-#--task-definition $task_def|grep ^DEPLOYMENTS
--- utils/set-task-def.py
+++ utils/set-task-def.py
@@ -5,14 +5,14 @@ from __future__ import print_function
 import json
 import sys
 
-name = sys.argv[1]
-image = sys.argv[2]
-git_sha = sys.argv[3]
+family = sys.argv[1]
+name = sys.argv[2]
+image = sys.argv[3]
 
 d = json.loads(sys.stdin.read())
 
+d['family'] = family
 d['containerDefinitions'][0]['name'] = name
 d['containerDefinitions'][0]['image'] = image
-d['containerDefinitions'][0]['dockerLabels']['git_sha'] = git_sha
 
 print(json.dumps(d, indent=4))
